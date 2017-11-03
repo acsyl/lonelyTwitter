215a62f92a1ef8b998b82e317350a2cb340343cf
acsyl <shuyang2@ualberta.ca>
2017-11-03 00:01:52 -0600

    delete unused assignment

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,7 +1,7 @@
 
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
 import android.widget.EditText;
@@ -21,7 +21,7 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2<
     }
 
     public void testStart() throws Exception {
-        Activity activity = getActivity();
+
     }
 
     public void setUp() throws Exception{
